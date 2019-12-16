b53fc724320625e0a2edd647f15665555e8985f0
Drumsid <den-sidnay@yandex.ru>
2019-12-16 21:00:34 +0300

    add routing

--- public/index.php
+++ public/index.php
@@ -8,7 +8,21 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
+$phones = [1,2,3];
+
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
+});
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('POST /users');
+});
+
+$app->get('/phone', function ($request, $response) use ($phones) {
+    return $response->write(json_encode($phones));
 });
 $app->run();
