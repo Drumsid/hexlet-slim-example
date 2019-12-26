288c9ab16600abb075066f21556a6c06e035b6b4
Denis <den-sidnay@yandex.ru>
2019-12-26 20:42:38 +0300

    add new routers users and templates

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+// подключаем контейнер для подключения шаблонов к slim
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория в которой будут храниться шаблоны
@@ -16,26 +17,41 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$phones = [1,2,3];
+$phones = [1, 2, 3];
 
+$users = [
+    ['id' => 32, 'firstName' => 'Julianne', 'lastName' => 'Mueller', 'email' => 'claire77@satterfield.com'],
+    ['id' => 54, 'firstName' => 'Brandy', 'lastName' => 'Reichel', 'email' => 'Reichel@satterfield.com'],
+    ['id' => 23, 'firstName' => 'Lonnie', 'lastName' => 'Ankunding', 'email' => 'Ankunding@satterfield.com'],
+    ['id' => 76, 'firstName' => 'Juwan', 'lastName' => 'Weimann', 'email' => 'Weimann@satterfield.com'],
+    ['id' => 2, 'firstName' => 'Norval', 'lastName' => 'Nitzsche', 'email' => 'Nitzsche@satterfield.com'],
+];
+
+// тестовый вывод slim при установке
+// $app->get('/', function ($request, $response) {
+//     return $response->write('Welcome to Slim!');
+// });
+
+//запрос на главную и подключение шаблона index в папке template
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 
+// get запрос
+//
 // $app->get('/users', function ($request, $response) {
 //     return $response->write('GET /users');
 // });
 //
+// post запрос
 // $app->post('/users', function ($request, $response) {
 //     return $response->write('POST /users');
 // });
+//
+//
 
 $app->get('/phone', function ($request, $response) use ($phones) {
     return $response->write(json_encode($phones));
-});
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
@@ -43,11 +59,34 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
     return $response->write("Course id: {$id}");
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+// запрос на /users и подключение шаблона users/index в папке template
+$app->get('/users', function ($request, $response, $args) use ($users) {
+    $params = ['users' => $users];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+// запрос на /users/{id} и подключение шаблона users/show в папке template
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $params = ['users' => $users, 'userId' => $args['id'], 'id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
+
+// 
+//
+// подключаем переадресацию на статус 302
+// $app->post('/users', function ($request, $response) {
+//     return $response->withStatus(302);
+// });
+// подключаем шаблон users/show и выводим переменные id и nickname
+// $app->get('/users/{id}', function ($request, $response, $args) {
+//     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+//     // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+//     // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+//     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+// });
+//
+//
+
+//
 
 $app->run();
